a131005dd4040ed8ef2306c9b73461fc597c09f9
satyamchourey <satyamchourey26@gmail.com>
2019-02-04 18:56:14 +0530

    Updates

---
+++ precache-manifest.f8b120003169fdac04346dbd4f6bf361.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25161f1b8def51123035dd0f02970464",
+    "url": "/devboard/static/media/Abstract.25161f1b.jpg"
+  },
+  {
+    "revision": "608e68d37faebb85696e",
+    "url": "/devboard/static/js/runtime~main.608e68d3.js"
+  },
+  {
+    "revision": "b35180396f70c2798b9b",
+    "url": "/devboard/static/js/main.b3518039.chunk.js"
+  },
+  {
+    "revision": "08d91d818ad8120e9203",
+    "url": "/devboard/static/js/1.08d91d81.chunk.js"
+  },
+  {
+    "revision": "fbf8f8bb4962a8fac99a4ec082c3428b",
+    "url": "/devboard/index.html"
+  }
+];
